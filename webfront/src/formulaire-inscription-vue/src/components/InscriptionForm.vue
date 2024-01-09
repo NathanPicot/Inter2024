4029f4dc67f9618a42e0c1118c94b6beed148072
GotoThelast <h.lafe@it-students.fr>
2024-01-09 16:33:43 +0100

    Page inscription

---
+++ webfront/src/formulaire-inscription-vue/src/components/InscriptionForm.vue
@@ -0,0 +1,39 @@
+<!-- src/components/Form.vue -->
+
+<template>
+  <h1 style="margin-left: 40%; margin-top: 10%">INSCRIPTION</h1>
+  <v-form @submit.prevent="submitForm" style="width: 70%; margin-left: 15%;">
+    <v-text-field v-model="formData.name" label="Pseudo" required style="width: 70%; margin-left: 15%;"></v-text-field>
+
+    <v-text-field v-model="formData.email" label="Email" required type="email" style="width: 70%; margin-left: 15%;"></v-text-field>
+
+    <v-text-field v-model="formData.password" label="Mot de passe" required type="password" style="width: 70%; margin-left: 15%;"></v-text-field>
+
+    <v-btn type="submit" color="primary" style="margin-left: 40%;">Inscription</v-btn>
+  </v-form>
+</template>
+
+<script>
+export default {
+  name: 'InscriptionForm',
+
+  data() {
+    return {
+      formData: {
+        name: '',
+        email: ''
+      }
+    };
+  },
+  methods: {
+    submitForm() {
+      // Envoyer les données du formulaire au backend (vous devrez implémenter cela)
+      console.log('Form submitted:', this.formData);
+    }
+  }
+};
+</script>
+
+<style scoped>
+/* Styles spécifiques au composant */
+</style>
