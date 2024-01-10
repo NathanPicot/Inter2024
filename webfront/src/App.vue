a36aade4e055a1bc8b0642bf5113f30eba5420f8
GotoThelast <h.lafe@it-students.fr>
2024-01-10 16:05:36 +0100

    Page inscription

---
+++ webfront/src/App.vue
@@ -0,0 +1,21 @@
+<template>
+  <div id="app">
+    <!-- Autres éléments de votre application -->
+    <FormInscription/>
+  </div>
+</template>
+
+<script>
+import FormInscription from "@/components/FormInscription.vue";  // Assurez-vous d'ajuster le chemin selon votre structure de fichiers
+
+export default {
+  components: {
+    FormInscription
+  },
+  // Autres configurations...
+};
+</script>
+
+<style>
+/* Styles globaux de l'application */
+</style>
---
+++ webfront/src/components/FormInscription.vue
@@ -0,0 +1,38 @@
+<!-- src/components/Form.vue -->
+
+<template>
+  <h1 class="d-flex justify-center mb-6 mt-10">INSCRIPTION</h1>
+  <div class="d-flex justify-center mb-6">
+    <v-form @submit.prevent="submitForm" class="w-25 justify-center mb-6">
+      <v-text-field v-model="formData.email" label="Email" required type="email"></v-text-field>
+      <v-text-field v-model="formData.name" label="Pseudo" required></v-text-field>
+      <v-text-field v-model="formData.password" label="Mot de passe" required type="password"></v-text-field>
+      <div class="d-flex justify-center mb-6">
+        <v-btn type="submit" color="primary">S'inscrire</v-btn>
+      </div>
+    </v-form>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      formData: {
+        name: '',
+        email: ''
+      }
+    };
+  },
+  methods: {
+    submitForm() {
+      // Envoyer les données du formulaire au backend (vous devrez implémenter cela)
+      console.log('Form submitted:', this.formData);
+    }
+  }
+};
+</script>
+
+<style scoped>
+/* Styles spécifiques au composant */
+</style>
