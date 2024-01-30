8225ac198620fb5e5039891b427c27793671a497
GotoThelast <h.lafe@it-students.fr>
2024-01-30 16:57:47 +0100

    Page inscription

--- webfront/src/components/FormInscription.vue
+++ webfront/src/components/FormInscription.vue
@@ -1,38 +1,153 @@
 <!-- src/components/Form.vue -->
 
 <template>
-  <h1 class="d-flex justify-center mb-6 mt-10">INSCRIPTION</h1>
-  <div class="d-flex justify-center mb-6">
-    <v-form @submit.prevent="submitForm" class="w-25 justify-center mb-6">
-      <v-text-field v-model="formData.email" label="Email" required type="email"></v-text-field>
-      <v-text-field v-model="formData.name" label="Pseudo" required></v-text-field>
-      <v-text-field v-model="formData.password" label="Mot de passe" required type="password"></v-text-field>
-      <div class="d-flex justify-center mb-6">
-        <v-btn type="submit" color="primary">S'inscrire</v-btn>
-      </div>
-    </v-form>
-  </div>
+  <v-row justify="center">
+    <v-col
+        cols="12"
+        sm="10"
+        md="8"
+        lg="6"
+    >
+      <v-card ref="form" id="formInscription">
+          <v-card-text class="text-center">
+            <div>
+              <h1 style="padding: 20px; margin-bottom: 20px">INSCRIPTION</h1>
+            </div>
+
+          <v-text-field
+              ref="username"
+              :rules="[v => !!v || 'This field is required']"
+              :error-messages="errorMessages"
+              label="Username"
+              placeholder="Username"
+              required
+              class="custom-background"></v-text-field>
+            <v-text-field
+                ref="mail"
+                :rules="[v => !!v || 'This field is required', (v) => /.+@.+\..+/.test(v) || 'E-mail must be valid']"
+                :error-messages="errorMessages"
+                label="E-mail"
+                placeholder="Enter your e-mail"
+                required
+                class="custom-background"
+            ></v-text-field>
+            <v-text-field
+                ref="verifmail"
+                :rules="[v => !!v || 'This field is required', (v) => /.+@.+\..+/.test(v) || 'E-mail must be valid']"
+                :error-messages="errorMessages"
+                label="Verification e-mail"
+                placeholder="Enter your e-mail"
+                required
+                class="custom-background"
+            ></v-text-field>
+            <v-text-field
+                ref="password"
+                :rules="[v => !!v || 'Password is required', (v) => (v && v.length >= 8) || 'Password must be at least 8 characters']"
+                :error-messages="errorMessages"
+                label="Password"
+                placeholder="Password"
+                required
+                type="password"
+                class="custom-background"
+            ></v-text-field>
+        </v-card-text>
+        <v-divider class="mt-1"></v-divider>
+        <v-card-actions class="justify-center mb-5">
+          <v-btn
+              rounded="lg"
+              @click="resetForm"
+              variant="text">
+            Cancel
+          </v-btn>
+
+          <v-btn
+              rounded="lg"
+              color="primary"
+              style="background-color: white"
+              variant="text"
+              @click="submit"
+          >
+            S'inscrire
+          </v-btn>
+        </v-card-actions>
+        <v-input__details v-if="!formHasErrors"></v-input__details>
+      </v-card>
+    </v-col>
+  </v-row>
 </template>
 
 <script>
+//import {Field} from "vee-validate";
+//import {useCounterStore} from '../store/userStore';
 export default {
-  data() {
-    return {
-      formData: {
-        name: '',
-        email: ''
+  //components: {Field},
+
+  computed: {
+    form () {
+      return {
+        name: this.name,
+        mail: this.mail,
+        password: this.password,
       }
-    };
+    },
   },
+
   methods: {
-    submitForm() {
-      // Envoyer les données du formulaire au backend (vous devrez implémenter cela)
-      console.log('Form submitted:', this.formData);
-    }
-  }
-};
+
+    resetForm() {
+      this.$refs.username.value = '';
+      this.$refs.mail.value = '';
+      this.$refs.verifmail.value = '';
+      this.$refs.password.value = '';
+      this.errorMessages = '';
+      this.formHasErrors = false;
+
+      /*Object.keys(this.form).forEach((f) => {
+        this.form[f] = '';
+      });*/
+    },
+
+    submit() {
+      console.log(this.$refs.username.value);
+      this.formHasErrors = false
+
+      //useCounterStore.increment()
+      //console.log(useCounterStore.state.count)
+      Object.keys(this.form).forEach(f => {
+        if (!this.form[f]) {
+          this.formHasErrors = true
+          this.errorMessages += `Hey! ${f.charAt(0).toUpperCase() + f.slice(1)} is required.\n`
+        }
+
+      })
+
+      // Nouvelle logique pour supprimer tous les messages d'erreur
+      this.errorMessages = ''
+    },
+  },
+}
 </script>
 
 <style scoped>
 /* Styles spécifiques au composant */
+#formInscription{
+  background-color: #5480D5;
+  margin-top: 50px;
+  border-radius: 20px;
+  padding: 30px;
+}
+
+.custom-background{
+  background-color: white;
+  //border: solid black 2px;
+  margin-bottom: 10px;
+  border-radius: 10px;
+  width: 60%;
+  margin-left: 20%;
+
+}
+
+v-btn{
+  border-radius: 60px;
+}
 </style>
