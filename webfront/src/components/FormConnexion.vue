8f90a75dc5758af113f2c273d1b287b0eb1f44f8
GotoThelast <h.lafe@it-students.fr>
2024-02-05 16:26:09 +0100

    page inscription + page connexion 2

---
+++ webfront/src/components/FormConnexion.vue
@@ -0,0 +1,135 @@
+<!-- src/components/Form.vue -->
+<script>
+export default {
+  data () {
+    return {
+      username: '',
+      modelPassword: '',
+      formValid: false,
+      show1: false,
+      show2: false,
+      password: 'Password',
+      rules: {
+        required: value => !!value || 'Required.',
+        min: v => v.length >= 8 || 'Min 8 characters',
+        // verifPassword: v => this.modelPassword.value !== v || 'Les mots de passes doivent être identiques !',
+      },
+    }
+  },
+  // created() {
+  //   this.rules.verifPassword= v => this.modelPassword.value !== v || 'Les mots de passes doivent être identiques !'
+  // },
+  computed: {
+    form() {
+      return {
+        name: this.name,
+        password: this.password,
+      };
+    },
+  },
+  methods: {
+    submit() {
+        console.log(this.formValid)
+
+    },
+  },
+};
+</script>
+
+
+<template>
+  <v-row justify="center">
+    <v-col
+        cols="12"
+        sm="10"
+        md="8"
+        lg="6"
+    >
+      <v-card ref="form" id="formInscription">
+        <v-form v-model="formValid">
+          <v-card-text class="text-center">
+
+            <div>
+              <h1 style="padding: 20px; margin-bottom: 20px; color: white; margin-top: 20px">CONNEXION</h1>
+            </div>
+            <v-text-field
+                ref="username"
+                v-model="username"
+                :rules="[v => !!v || 'Ce champ est requis !!']"
+                label="Nom d'utilisateur ou E-mail"
+                placeholder="Entrez votre nom d'utilisateur ou E-mail"
+                required
+                class="custom-background elevation-5"></v-text-field>
+            <v-text-field
+                ref="password"
+                v-model="modelPassword"
+                :append-icon="show1 ? 'mdi-eye' : 'mdi-eye-off'"
+                :type="show1 ? 'text' : 'password'"
+                name="input-10-1"
+                :rules="[rules.required, rules.min]"
+                label="Mot de passe"
+                placeholder="Entrez votre mot de passe"
+                required
+                counter
+                @click:append="show1 = !show1"
+                class="custom-background elevation-5"
+            ></v-text-field>
+          </v-card-text>
+          <v-card-actions class="justify-center mb-8">
+            <v-btn
+                rounded="lg"
+                color="primary"
+                style="background-color: white"
+                variant="text"
+                :disabled="!formValid"
+                @click="submit"
+            >
+              Connexion
+            </v-btn>
+
+            <v-btn
+                style="color: white"
+                rounded="lg"
+                to="/FormInscription"
+                variant="text">
+              S'inscrire
+            </v-btn>
+          </v-card-actions>
+        </v-form>
+      </v-card>
+    </v-col>
+  </v-row>
+</template>
+
+
+
+<style scoped>
+/* Styles spécifiques au composant */
+#formInscription{
+  background-color: #022037;
+  margin-top: 80px;
+  border-radius: 20px;
+  padding: 5px;
+}
+
+.custom-background{
+  background-color: white;
+  //border: solid black 2px;
+  margin-bottom: 20px;
+  border-radius: 10px;
+  padding: 5px;
+  width: 60%;
+  margin-left: 20%;
+
+}
+
+body {
+  overflow-y: hidden;
+  background-color: aliceblue;
+}
+
+v-btn{
+  border-radius: 60px;
+}
+
+</style>
